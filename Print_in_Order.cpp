76b2075d03a68b3fd85140da9d49ec5357d81763
Ivan Shirokikh <playing.and.programmirovanie@gmail.com>
2024-07-01 21:17:44 +0300

    creat gitignore

---
+++ Print_in_Order.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <functional>
+//
+//using namespace std;
+//
+//class Foo {
+//public:
+//    Foo() {
+//
+//    }
+//
+//    void first(function<void()> printFirst) {
+//
+//        // printFirst() outputs "first". Do not change or remove this line.
+//        printFirst();
+//    }
+//
+//    void second(function<void()> printSecond) {
+//
+//        // printSecond() outputs "second". Do not change or remove this line.
+//        printSecond();
+//    }
+//
+//    void third(function<void()> printThird) {
+//
+//        // printThird() outputs "third". Do not change or remove this line.
+//        printThird();
+//    }
+//};
+//
+//int main()
+//{
+//	//system("chcp 1251");
+//	setlocale(LC_ALL, "Russian");
+//
+//	return 0;
+//}
